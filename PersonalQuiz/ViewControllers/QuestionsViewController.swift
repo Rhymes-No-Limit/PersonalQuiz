a88973555696171167c2ee9bd044db364391ef2e
Andrey <Rhymes.un.limit@gmail.com>
2025-05-25 16:10:44 +0300

    add model

--- PersonalQuiz/ViewControllers/QuestionsViewController.swift
+++ PersonalQuiz/ViewControllers/QuestionsViewController.swift
@@ -1,6 +1,8 @@
 import UIKit
 
 class QuestionsViewController: UIViewController {
+    
+    private let question = Question.getQuestions()
 
     override func viewDidLoad() {
         super.viewDidLoad()
